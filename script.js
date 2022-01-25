c1194e2fe6bacb27e72e6a86e80e70a8ecd4bb19
CDDC <cynthia.decelle@gmail.com>
2022-01-25 16:50:20 +0100

    JS Creating variables of needed data from fetched API's, EventListener on SubmitBtn

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ const submitBtn = document.querySelector("#submitcity");
 // Add city for API call
 const handleForm = (event) => {
     event.preventDefault();
-
     // API call
-    const getData = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + form.value + "&appid=" + Data.key)
+    const getCoordinates = fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +form.value + "&appid=" + Data.key)
         .then(response => response.json())
-        .then(data => console.log(data))
+            .then(data => {
+                const latitude = data[0].lat;
+                const longitude = data[0].lon;
+                const getWeatherData = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+ latitude + "&lon=" + longitude + "&units=metric" + "&appid=" + Data.key)
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        console.log(data)
+                    })
+                
+            })
+        
+        
 }
-
 submitBtn.addEventListener('click', handleForm);
+
+
+
