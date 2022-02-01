16da79021ee93a943561909f3646f3bbbf62758a
CDDC <cynthia.decelle@gmail.com>
2022-02-01 17:05:57 +0100

    JS refact.: create element-function, separated embeded functions, polished general structure

--- script.js
+++ script.js
@@ -1,28 +1,79 @@
 // import Data from "./config.js";
+
 const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const form = document.querySelector(".cityfield");
 const submitBtn = document.querySelector("#submitcity");
+
+const pressEnter = (event) => {
+    const pressedKey = event.key;
+    if (pressedKey == "Enter") {
+        handleForm();
+    };
+}
+
+// Add city for API call
+const handleForm = (event) => {
+    //to only display the new data, remove childelement(ul) from parenthtml element
+    let mainParent = document.querySelector('#card');
+    while (mainParent.firstChild) {
+        mainParent.removeChild(mainParent.firstChild);
+    }
+    // Geocode API call to get the coordinates needed for weather API
+    fetchCoordinates(form.value);
+}
+
+// const inputCity = form.value
+const fetchCoordinates = (inputCity) => {
+    const getCoordinates = fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +inputCity + "&appid=a790165930e5b592de2330f642ceff0c")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            const latitude = data[0].lat;
+            const longitude = data[0].lon;
+            //API call with coordinates
+            fetchWeatherData(latitude, longitude); //refers to the function fetchWeatherData
+        })
+}
+
+const fetchWeatherData = (lat, long) => {
+    const getWeatherData = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon=" + long + "&units=metric" + "&appid=a790165930e5b592de2330f642ceff0c")
+        .then(response => response.json())
+        .then(data => {// data = the whole data fetched from the API.
+            console.log(data)
+            const dailyWeather8 = data.daily;//array of 8 objects. Objects are the weatherinfo per day.
+            const dailyWeather5 = dailyWeather8.slice(0, 5);//takes the first 5 objects of the array above starting from position 0.
+            const mainHtml = document.querySelector('#card');
+            for (let day of dailyWeather5) {
+                createDay(mainHtml, day); //refers to the function createDay
+            }
+        })
+}
+
+// function pre-creating html elements
+const createElement = (tagN, idN, classN, source, innerHtml, parent) => {
+    const tagName = document.createElement(tagN);
+    tagName.id = idN;
+    tagName.className = classN;
+    tagName.src = source;
+    tagName.innerHTML = innerHtml;
+    parent.appendChild(tagName);
+
+    return tagName;
+};
+
+// function creating proper html elements for each day
 const createDay = (mainHtml, day) => {
-    //TODO: try with html template literal. Don't forget ; after closing backtick?
-    const ulList = document.createElement("ul");
-    ulList.className = "daily-card";
-    mainHtml.appendChild(ulList);
+    //TODO: const nodige data
+    const ulList = createElement('ul', null, 'daily-card','', '', mainHtml);
 
-    const firstSection = document.createElement("section");
-    firstSection.id = "card-head";
-    ulList.appendChild(firstSection);
+    const firstSection = createElement('section', 'card-head', '', '', '', ulList);
 
-    //TODO: transform wind-degrees in compass (letters)
     const weatherIcon = day.weather[0].icon;
-    const iconLi = document.createElement("li");
-    iconLi.className = "weather-icon";
-    firstSection.appendChild(iconLi);
-
-    const iconImg = document.createElement('img')
-    iconImg.src = "http://openweathermap.org/img/wn/" + weatherIcon +"@2x.png";
-    iconImg.className = "icon";
-    iconLi.appendChild(iconImg);
-
+    const iconLi = createElement('li', '', 'weather-icon','', '', firstSection);
+    
+    const iconImg = createElement('img', '', 'icon', "http://openweathermap.org/img/wn/" + weatherIcon +"@2x.png", '', iconLi);
+    
     const unixDate = day.dt;
     const dateJSconversion = new Date(unixDate*1000);
     const weekDay = dateJSconversion.getDay();
@@ -32,77 +83,27 @@ const createDay = (mainHtml, day) => {
     const minTemp = day.temp.min;
     const maxTemp = day.temp.max;
 
-    const cardDayandTemp = document.createElement("li");
-    cardDayandTemp.className = "day";
-    cardDayandTemp.innerHTML = dayOfWeek + "<br>" + date + "<br>" + "<span>" + Math.round(minTemp) + "°/ " + 
-    Math.round(maxTemp) + "°" + "</span>";
-    firstSection.appendChild(cardDayandTemp);
+    const cardDayandTemp = createElement('li', '', 'day', '', dayOfWeek + "<br>" + date + "<br>" + "<span>" + Math.round(minTemp) + 
+    "°/ " + Math.round(maxTemp) + "°" + "</span>", firstSection);
 
-    const secondSection = document.createElement("section");
-    secondSection.id = "card-body";
-    ulList.appendChild(secondSection);
+    const secondSection = createElement('section', 'card-body', '', '', '', ulList);
 
     const humidity = day.humidity;
-    const humid = document.createElement("li");
-    humid.className = "humidity";
-    humid.innerHTML = "Humidity " + humidity + "%";
-    secondSection.appendChild(humid);
+    const humid = createElement('li', '', 'humidity', '',"Humidity " + humidity + "%", secondSection);
 
     const precipitationProb = day.pop;
-    const precipitationPr= document.createElement("li");
-    precipitationPr.className = "precipitation-prob";
-    precipitationPr.innerHTML = "Rain " + precipitationProb + "%";
-    secondSection.appendChild(precipitationPr);
+    const precipitationPr = createElement('li', '', 'precipitation-prob', '', "Rain " + precipitationProb + "%", secondSection);
 
     const windSpeed = day.wind_speed;
-    const windSp = document.createElement("li");
-    windSp.className = "wind-speed";
-    windSp.innerHTML = "Wind " + Math.round(windSpeed) + " km/h";
-    secondSection.appendChild(windSp);
+    const windSp = createElement('li', '', 'wind-speed', '', "Wind " + Math.round(windSpeed) + " km/h", secondSection);
 
     const windDirectionDegree = day.wind_deg;
-    const windDirection= document.createElement("li");
-    windDirection.className = "wind-direction";
-    windDirection.innerHTML = "Wind.d. " + windDirectionDegree;
-    secondSection.appendChild(windDirection);
+    const windDirection = createElement('li', '', 'wind-direction', '', "Wind.d. " + windDirectionDegree, secondSection);
 }
-// Add city for API call
-const handleForm = (event) => {
-    //to only display the new data, remove childelement(ul) from parenthtml element
-    let mainParent = document.querySelector('#card');
-    while (mainParent.firstChild) {
-        mainParent.removeChild(mainParent.firstChild);
-    }
-    // Geocode API call to get the coordinates needed for weather API
-    const getCoordinates = fetch("http://api.openweathermap.org/geo/1.0/direct?q=" +form.value + "&appid=a790165930e5b592de2330f642ceff0c")
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            const latitude = data[0].lat;
-            const longitude = data[0].lon;
-            //API call with coordinates
-            const getWeatherData = fetch ("https://api.openweathermap.org/data/2.5/onecall?lat="+ latitude + "&lon=" + longitude + "&units=metric" + "&appid=a790165930e5b592de2330f642ceff0c")
-                .then(response => response.json())
-                .then(data => {// data = the whole data fetched from the API.
-                    const dailyWeather8 = data.daily;//array of 8 objects. Objects are the weatherinfo per day.
-                    const dailyWeather5 = dailyWeather8.slice(0, 5);//takes the first 5 objects of the array above starting from position 0.
 
-                    const mainHtml = document.querySelector('#card');
-                    
-                    for (let day of dailyWeather5) {
-                        createDay(mainHtml, day);
-                    }
-                })
-            
-        })
-}
 submitBtn.addEventListener('click', handleForm);
 
-const pressEnter = (event) => {
-    const pressedKey = event.key;
-    if (pressedKey == "Enter") {
-        handleForm();
-    };
-}
 form.addEventListener('keydown', pressEnter);
+
+ //TODO: try with html template literal. Don't forget ; after closing backtick?
+//TODO: transform wind-degrees in compass (letters)
